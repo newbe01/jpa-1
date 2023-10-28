b644b830287e8b2824222db1beb68a5f50e0ae17
newbe01 <pkwaa3@gmail.com>
2023-10-28 20:21:35 +0900

    database connection

---
+++ src/main/java/jpabook/jpa/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+}
