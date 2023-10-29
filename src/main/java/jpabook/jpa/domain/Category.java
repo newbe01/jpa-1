3dc05976efbdc9a2b9f964b5625d83fafa76eb1c
newbe01 <pkwaa3@gmail.com>
2023-10-29 23:20:32 +0900

    relation method, cascade

--- src/main/java/jpabook/jpa/domain/Category.java
+++ src/main/java/jpabook/jpa/domain/Category.java
@@ -33,4 +33,10 @@ public class Category {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    // relation method
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
 }
