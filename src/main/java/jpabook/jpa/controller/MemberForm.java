42726816622bcdf456ecb9b3013f75facff257cd
newbe01 <pkwaa3@gmail.com>
2023-10-31 23:53:49 +0900

    member  sign up

---
+++ src/main/java/jpabook/jpa/controller/MemberForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpa.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+public class MemberForm {
+
+    @NotEmpty
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
