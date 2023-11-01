4ce113412abe1f95a624479c8142c9f74528190f
newbe01 <pkwaa3@gmail.com>
2023-11-01 22:48:00 +0900

    item order

---
+++ src/main/java/jpabook/jpa/controller/OrderController.java
@@ -0,0 +1,45 @@
+package jpabook.jpa.controller;
+
+import jpabook.jpa.domain.Member;
+import jpabook.jpa.domain.item.Item;
+import jpabook.jpa.service.ItemService;
+import jpabook.jpa.service.MemberService;
+import jpabook.jpa.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count)
+    {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
